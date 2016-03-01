2ab9cfe9e910965956e87d9dd65c1ff9c8de6cd5
Christian Salas <christiansalas@Christians-MacBook-Air.local>
2016-02-29 16:40:25 -0800

    javascript button

---
+++ main2.js
@@ -0,0 +1,10 @@
+var fn = function () { 
+	console.log('heeey')
+} 
+var beeper = function(){ 
+	document.body.append
+} 
+
+document.querySelector('#target').addEventListener('click', function() { 
+	console.log('same deal') 
+})
